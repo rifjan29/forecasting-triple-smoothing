8e144d0da0ed9719815bbf9ab0bd4f60b2f36c1c
Rifjan Jundila <rjndla@gmail.com>
2022-10-09 23:58:14 +0700

    Templating

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  <link href="{{ asset('backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('backend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('backend/css/ruang-admin.min.css') }}" rel="stylesheet">
+  @stack('css')
+</head>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    @include('layouts._partials.sidebar')
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+        @include('layouts._partials.topbar')
+        <!-- Topbar -->
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+        <!-- Container Fluid-->
+        @yield('content')
+        <!---Container Fluid-->
+      </div>
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto py-2">
+          <div class="copyright text-center my-auto">
+            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - distributed by
+              <b><a href="https://themewagon.com/" target="_blank">themewagon</a></b>
+            </span>
+          </div>
         </div>
-    </body>
+      </footer>
+      <!-- Footer -->
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="{{ asset('') }}backend/vendor/jquery/jquery.min.js"></script>
+  <script src="{{ asset('') }}backend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('') }}backend/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="{{ asset('') }}backend/js/ruang-admin.min.js"></script>
+  <script src="{{ asset('') }}backend/vendor/chart.js/Chart.min.js"></script>
+  <script src="{{ asset('') }}backend/js/demo/chart-area-demo.js"></script>
+  @stack('js')
+</body>
+
 </html>
